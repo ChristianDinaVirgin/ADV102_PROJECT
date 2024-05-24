c9b9305869e62437cf5de5b491de019e94cca8dd
ChristianDinaVirgin <130618875+ChristianDinaVirgin@users.noreply.github.com>
2024-05-24 23:31:04 +0800

    ADV102
    ADV102

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      bgColor: '#18191a',
+      boxColor: '#242526',
+      btn: '#0866ff',
+      bgLogin: '#f0f2f5',
+    },
+  },
+  plugins: [],
+});
